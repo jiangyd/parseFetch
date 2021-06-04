0dcbd35b8df5c1de9a26f69cda2e3aec6b749df8
jiangyd <962584902@qq.com>
2021-06-04 21:02:59 +0800

    增加使用说明

---
+++ example/login.js
@@ -0,0 +1,15 @@
+fetch("http://testing.tester.cn/api/v1/auth-token/login", {
+  "headers": {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "zh-CN,zh;q=0.9",
+    "cache-control": "no-cache",
+    "content-type": "application/json;charset=UTF-8",
+    "pragma": "no-cache",
+    "x-ft-auth-token": ""
+  },
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": "{\"username\":\"jiangyd\",\"password\":\"123456\"}",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "omit"
+});
